3a6e2cb6100e2a3ca170504bd7916ef2b94304f9
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-02-11 14:28:22 +0200

    refactor: config file setup.

--- vendors/postgres/config_file_setup.py
+++ vendors/postgres/config_file_setup.py
@@ -4,14 +4,21 @@ from vendors.postgres.config_file_handler.file_handler import FileHandler
 class ConfigFileSetup:
     def __init__(self, file_path: str):
         self.file_path = file_path
+        self.config_file = FileHandler(file_path=self.file_path)
 
     def execute(self):
-        config_file = FileHandler(file_path=self.file_path)
+        self.enable_and_set_wal_level_to_archive()
+        self.enable_and_set_archive_mode_to_on()
+        self.enable_and_set_archive_command()
 
-        config_file.set_directive_value('wal_level', 'archive')
-        config_file.set_directive_value('archive_mode', 'on')
+        self.config_file.save()
 
+    def enable_and_set_wal_level_to_archive(self) -> None:
+        self.config_file.set_directive_value(name='wal_level', value='archive')
+
+    def enable_and_set_archive_mode_to_on(self) -> None:
+        self.config_file.set_directive_value('archive_mode', 'on')
+
+    def enable_and_set_archive_command(self) -> None:
         archive_command = 'test ! -f /var/lib/postgresql/pg_log_archive/%f && cp %p /var/lib/postgresql/pg_log_archive/%f'
-        config_file.set_directive_value('archive_command', archive_command, True)
-
-        config_file.save()
+        self.config_file.set_directive_value('archive_command', archive_command, True)
