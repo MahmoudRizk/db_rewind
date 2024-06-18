8d720f2d3c8f74feea7f3f81562b2e4b4cd02e19
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-06-18 19:49:11 +0300

    Add unti test to config line class Fix bugs realted to comment parsing in config line class.

---
+++ tests/unit/test_config_line.py
@@ -0,0 +1,68 @@
+import unittest
+from parameterized import parameterized
+
+from vendors.postgres.config_file_handler.config_line import ConfigLine
+
+
+class TestConfigLine(unittest.TestCase):
+    
+    @parameterized.expand([
+        ["#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file"],
+        ["hba_file = ''", "hba_file = ''"]
+    ])
+    def test_get_line(self, line: str, expected: str):
+        config_line = ConfigLine(line)
+
+        self.assertEqual(expected, config_line.get())
+    
+    @parameterized.expand([
+        ["#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "hba_file"],
+        ["#hba_file = 'ConfigDir/pg_hba.conf'        # host-based authentication file        ", "hba_file"],
+        ["hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "hba_file"],
+        ["hba_file = 'ConfigDir/pg_hba.conf'", "hba_file"]
+    ])
+    def test_get_directive_name(self, line: str, expected: str):
+        config_line = ConfigLine(line)
+        self.assertEqual(expected, config_line.get_directive_name())
+    
+    @parameterized.expand([
+        ["#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file"],
+        ["hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file"],
+        ["hba_file = 'ConfigDir/pg_hba.conf'", "hba_file = 'ConfigDir/pg_hba.conf'"]
+    ])
+    def test_enable_directive(self, line: str, expected: str):
+        config_line = ConfigLine(line)
+        self.assertEqual(expected, config_line.enable_directive().get())
+    
+    @parameterized.expand([
+        ["#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file"],
+        ["hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file"],
+        ["hba_file = 'ConfigDir/pg_hba.conf'", "#hba_file = 'ConfigDir/pg_hba.conf'"]
+    ])
+    def test_disable_directive(self, line: str, expected: str):
+        config_line = ConfigLine(line)
+        self.assertEqual(expected, config_line.disable_directive().get()) 
+    
+    @parameterized.expand([
+        ["#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", '/home/hba.conf', True, "hba_file = '/home/hba.conf' # host-based authentication file"],
+        ["#hba_file = 'ConfigDir/pg_hba.conf'    # host-based authentication file", '/home/hba.conf', True, "hba_file = '/home/hba.conf' # host-based authentication file"],
+        ["#hba_file = 'ConfigDir/pg_hba.conf'", '/home/hba.conf', True, "hba_file = '/home/hba.conf'"],
+        ["#load_timeout = '0'", '123', False, "load_timeout = 123"],
+        ["#load_timeout = 99", '456', False, "load_timeout = 456"],
+    ])
+    def test_set_directive_value(self, line: str, value: str, use_single_quotation: bool, expected: str):
+        config_line = ConfigLine(line)
+        self.assertEqual(expected, config_line.set_directive_value(value, use_single_quotation).get())
+    
+
+    @parameterized.expand([
+        ["#hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "hba_file", True],
+        ["hba_file = 'ConfigDir/pg_hba.conf' # host-based authentication file", "hba_file", True],
+        ["hba_file = 'ConfigDir/pg_hba.conf'", "hba_file", True],
+        ["#hba_file = 'ConfigDir/pg_hba.conf'", "hba_file", True],
+    ])
+    def test_has_directive(self, line: str, directive_name: str, expected: bool):
+        config_line = ConfigLine(line)
+        self.assertEqual(expected, config_line.has_directive(directive_name))        
+    
+    
--- vendors/postgres/config_file_handler/config_line.py
+++ vendors/postgres/config_file_handler/config_line.py
@@ -22,8 +22,8 @@ class ConfigLine:
     def set_directive_value(self, value: any, use_single_quotation: bool) -> "ConfigLine":
         directive_name: str = self.get_directive_name()
         if directive_name:
-            _line = f"{directive_name} = " + (f"'{value}'" if use_single_quotation else f"{value}")
-            self.line = _line + ' ' + self._get_line_comment()
+            _line = f"{directive_name} = " + self._parse_set_directive_value(value, use_single_quotation)
+            self.line = self._append_comment_if_any(_line, self._get_line_comment())
         return self
 
     def has_directive(self, name: str) -> bool:
@@ -31,5 +31,19 @@ class ConfigLine:
 
     def _get_line_comment(self) -> str:
         # Finds second occurrence of '#', then return sub string starting from its occurrence.
-        return self.line[self.line.find('#', 1):]
+        index = self.line.find('#', 1)
+        
+        # no comment found.
+        if index == -1:
+            return ''
+        
+        return self.line[index:].strip()
+
+    def _append_comment_if_any(self, line: str, comment: str) -> str:
+        if comment:
+            line = line + ' ' + comment
+        return line
+    
+    def _parse_set_directive_value(self, value: any, use_single_quotation: bool) -> str:
+        return f"'{value}'" if use_single_quotation else f"{value}"
 
