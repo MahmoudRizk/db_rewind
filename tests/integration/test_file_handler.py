59f3ec38b3da7b314a125f4b5739d8026ab4f49e
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-06-19 04:49:01 +0300

    fix: failing integration test for file handler test.

--- tests/integration/test_file_handler.py
+++ tests/integration/test_file_handler.py
@@ -1,5 +1,4 @@
 import unittest
-import difflib
 
 from pathlib import Path
 
@@ -15,11 +14,21 @@ class TestFileHandler(unittest.TestCase):
         file_handler.set_directive_value('archive_command', "test ! -f /var/lib/postgresql/wal_backup_12/%f && cp %p /var/lib/postgresql/wal_backup_12/%f", True)
         file_handler.set_directive_value('wal_level', 'archive')
 
+        # Enable disabled directive
+        file_handler.enable_directive('enable_async_append')
+        
+        # Disable enabled directive
+        file_handler.disable_directive('exit_on_error')
+
+        # Disable already disabled directive
+        file_handler.disable_directive('include_dir')
+
         in_memory_file = file_handler.save(in_memory=True)
 
         with open(THIS_DIR / 'fixtures/expected_postgres.conf') as expected:
             expected_file = expected.readlines()
 
-        for it1, it2 in zip(in_memory_file, expected_file):
-            # print(it1, it2)
-            self.assertEqual(it1, it2)
+        for line1, line2 in zip(in_memory_file, expected_file):
+            self.assertEqual(line1, line2)
+
+        self.assertEqual(len(in_memory_file), len(expected_file), f"{in_memory_file}\n\n{expected_file}")
