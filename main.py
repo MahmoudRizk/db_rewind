a0fcbcfb01068eaf0191040fb7f4145642ab15f0
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-06-22 02:34:49 +0300

    feat: add traditional cli interface with arguments parser.

--- main.py
+++ main.py
@@ -1,12 +1,75 @@
+import argparse
+import os
+
 from dotenv import load_dotenv
 
 from db_rewind.console.menu.main import Main as ConsoleMain
 
 from prompt_toolkit import PromptSession
 
+from db_rewind.postgres.db_rewinder import DBRewinder
+from db_rewind.postgres.db_setup import DBSetup
+
 
 def main():
     load_dotenv()
+
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(description='Command line tool')
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # Create the parser for the "rewind" command
+    parser_rewind = subparsers.add_parser('rewind', help='Rewind the database')
+    parser_rewind.add_argument('--db-rewind-date', type=str, help='Optional date to rewind the database to')
+    parser_rewind.add_argument('--disable-user-interaction', action='store_true', help='Disable user interaction')
+
+    # Create the parser for the "setup" command
+    parser_setup = subparsers.add_parser('setup', help='Setup the database')
+    parser_setup.add_argument('--disable-user-interaction', action='store_true', help='Disable user interaction')
+
+    # Start interactive cli
+    subparsers.add_parser('interactive', help='Start interactive cli')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    if args.command == 'rewind':
+        handle_rewind(args)
+    elif args.command == 'setup':
+        handle_setup(args)
+    elif args.command == 'interactive':
+        handle_interactive(args)
+
+
+def handle_rewind(args):
+    load_dotenv()
+    if args.disable_user_interaction:
+        print("User interaction is disabled for rewind.")
+        os.environ['DB_REWINDER_ALLOW_USER_HANDLE_ERROR_MANUALLY'] = '0'
+        os.environ['DB_REWINDER_ALLOW_INPUT_PROMPTS'] = '0'
+
+    if args.db_rewind_date:
+        print(f"Rewinding database to {args.db_rewind_date}.")
+        os.environ['DB_REWINDER_REWIND_DATE'] = args.db_rewind_date
+    else:
+        print("Rewinding database to the default date from .env file")
+
+    DBRewinder.execute()
+
+
+def handle_setup(args):
+    if args.disable_user_interaction:
+        print("User interaction is disabled for setup.")
+        os.environ['DB_REWINDER_ALLOW_USER_HANDLE_ERROR_MANUALLY'] = '0'
+        os.environ['DB_REWINDER_ALLOW_INPUT_PROMPTS'] = '0'
+
+    print("Setting up the database...")
+    DBSetup.execute()
+
+
+def handle_interactive(args):
+    os.environ['DB_REWINDER_ALLOW_USER_HANDLE_ERROR_MANUALLY'] = '1'
+    os.environ['DB_REWINDER_ALLOW_INPUT_PROMPTS'] = '1'
 
     session = PromptSession()
 
