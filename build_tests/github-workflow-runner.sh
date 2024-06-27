df0c1e5f4e00ed2ca83030e72492eefdb29f7862
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-06-27 20:31:48 +0300

    fix: github test runner.

--- build_tests/github-workflow-runner.sh
+++ build_tests/github-workflow-runner.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Simulate a command's output
-output=$(sudo docker-compose -f ./build_tests/docker-compose.yml up --exit-code-from db-rewind-container)
-echo $output
-
-# Check if "exited with code 0" exists in the output
-if echo "$output" | grep -q "exited with code 0"; then
-  echo "E2E test done successfully."
-  exit 0
-else
-  # If the string is not found, exit with code 1
-  echo "Error: E2E test failed using docker compose."
-  exit 1
-fi
+docker-compose -f ./build_tests/docker-compose.yml up --exit-code-from db-rewind-container
+exit $?
