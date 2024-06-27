37d15f86d9530a6ca6bcc27dd7427ecaa2522886
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-06-27 19:40:12 +0300

    feat: use docker volume mount instead of git clone.

--- build_tests/src/entrypoint.sh
+++ build_tests/src/entrypoint.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-# Clone db_rewind repo
-git clone https://github.com/MahmoudRizk/db_rewind /opt/db_rewind
-
-# Pull latest from remote repo
-git -C /opt/db_rewind pull
-
 # Install pip requirements
 pip install -r /opt/db_rewind/requirements.txt
 
 # Copy env file
-cp /opt/db_rewind.env /opt/db_rewind/.env
+cp /opt/db_rewind/build_tests/src/db_rewind.env /opt/db_rewind/.env
 
 # Start the PostgreSQL service
 service postgresql start
 
 # Create database & test data
-su - postgres -c "psql -f /opt/setup_database.sql"
+su - postgres -c "psql -f /opt/db_rewind/build_tests/src/setup_database.sql"
 
 # Setup Database configuration for database rewinding
-python3 /opt/db_rewind_test.py
+python3 /opt/db_rewind/build_tests/src/db_rewind_test.py
