bfb6b0278095d699bac5c7efaeaa903a3b87745b
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-06-28 05:31:23 +0300

    feat: control postgres version from github-workflow-runner file.

--- build_tests/github-workflow-runner.sh
+++ build_tests/github-workflow-runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker-compose -f ./build_tests/docker-compose.yml up --exit-code-from db-rewind-container
+export DB_REWINDER_POSTGRES_VERSION="$1"; 
+docker-compose -f ./build_tests/docker-compose.yml up --exit-code-from db-rewind-container;
 exit $?
--- build_tests/src/entrypoint.sh
+++ build_tests/src/entrypoint.sh
@@ -8,6 +8,9 @@ pip install -r /opt/db_rewind/requirements.txt
 # Copy env file
 cp /opt/db_rewind/build_tests/src/db_rewind.env /opt/db_rewind/.env
 
+# Replace string place holder with postgres version defined in env
+sed -i "s/{DB_REWINDER_POSTGRES_VERSION}/$DB_REWINDER_POSTGRES_VERSION/" /opt/db_rewind/.env 
+
 # Start the PostgreSQL service
 service postgresql start
 
